ecf7d7dc12356e878e5511db38a293994a823075
Erick <libertti@live.com>
2021-06-22 02:07:58 -0300

    okr

---
+++ src/components/dashboard/index.jsx
@@ -0,0 +1,8 @@
+import '../../styles/dashboard/dashboard.scss';
+export function Dashboard(props){
+    return (
+        <div id="dashboard">
+            {props.children}
+        </div>
+    )
+}
