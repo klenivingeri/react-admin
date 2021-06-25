4e84f2ac979659e48b8bb2a8b5aec99df16ce003
Erick <libertti@live.com>
2021-06-25 13:23:15 -0300

    obj da tabela

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -137,6 +137,72 @@ export function Table(){
                 }]
             }]
         }]
+    },{
+        title: 'Base de Clientes - DVR',
+        type: 'DVR',
+        list: '3',
+        thm: [{
+            title: 'Base de Clientes - THM',
+            type: 'THM',
+            list: '3.1',
+            okr: [{
+                title: 'Base de Clientes - OKR',
+                type: 'OKR',
+                list: '3.1.1', 
+                priority: '1000',   
+                kr:[{
+                    list: '3.1.1.1',
+                    type: 'KR',
+                    goal: 'Base ativa de clientes recorrentes',
+                    team: 'Corp',
+                    owner: 'Fred',
+                    priority: '400',
+                    low: '10,0',
+                    medium: '11,0',
+                    high: '12,0',
+                    metric: 'Soma mensal',
+                    status: 'Approved'
+                },
+                {
+                    list: '3.1.1.2',
+                    type: 'KR',
+                    goal: 'Venda de novas lojas',
+                    team: 'Corp',
+                    owner: 'Fabrício',
+                    priority: '300',
+                    low: '10,0',
+                    medium: '11,0',
+                    high: '12,0',
+                    metric: 'Média',
+                    status: 'Review'
+                },
+                {
+                    list: '3.1.1.3',
+                    type: 'KR',
+                    goal: 'Venda total Mobile Remoto',
+                    team: 'Corp',
+                    owner: 'Fabrício',
+                    priority: '100',
+                    low: '10,0',
+                    medium: '11,0',
+                    high: '12,0',
+                    metric: 'Padrão',
+                    status: 'Pending'
+                },{
+                    list: '3.1.1.4',
+                    type: 'KR',
+                    goal: 'Venda total Mobile Remoto',
+                    team: 'Operações',
+                    owner: 'Fabrício',
+                    priority: '0',
+                    low: '10,0',
+                    medium: '11,0',
+                    high: '12,0',
+                    metric: 'Total Anual',
+                    status: 'Rejected'
+                }]
+            }]
+        }]
     }]
     
     return(
